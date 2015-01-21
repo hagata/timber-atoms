5c57c3eed97cb00608dceb1573beffd5cccb876a
genki <genki@hagata.info>
2015-01-20 21:38:37 -0800

    adds gulp concat and atomizes scss

---
+++ gulpfile.js
@@ -0,0 +1,25 @@
+// 
+var gulp = require('gulp'),
+	concat = require('gulp-concat');
+
+//path variables
+var sassSources = ['sass/*/*/*.scss','sass/*/*.scss','sass/*.scss'],
+	shopifyAssets = 'assets/';
+
+//tasks
+
+//This combines all the molecule level scss files and puts them into the assets/ folder to be used in out shopify theme
+gulp.task('concat', function(){
+	gulp.src(sassSources)
+		.pipe(concat('_lumberjack.scss.css')) //underscores so its at the top
+		.pipe(gulp.dest(shopifyAssets))
+});
+
+// watch 
+
+gulp.task('watch', function(){
+	gulp.watch(sassSources,['concat'])
+});
+
+//default
+gulp.task('default',['concat','watch']);
